96e07a8e0e1e29d44d838c9b304d9264b684a839
gigi <giselaoliva99@hotmail.com>
2023-04-09 13:08:22 -0300

    last c

---
+++ dados.js
@@ -0,0 +1,23 @@
+
+//funcion para el numero del jugador//
+//document.getElementById("dadosjugador").addEventListener("click", numeroRandomjugador);
+
+function numeroRandomjugador(){
+    Math.floor(Math.random() *12 + 1);
+    }
+    
+
+//funcion para el resultado de la pc//
+
+
+
+//function resultado(){
+    
+//}
+
+//funcion para el mensaje a recibir dependiendo del resultado
+//if: mayor que la pc
+//else: menor que la pc
+
+//let numero = document.getElementById()
+
