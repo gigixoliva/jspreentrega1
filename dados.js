7e2b3b642c28e88e402a822fe03fc9bf8bdf0191
gigi <giselaoliva99@hotmail.com>
2023-04-09 21:20:33 -0300

    quite console logs

--- dados.js
+++ dados.js
@@ -8,25 +8,20 @@ let numeroRandompc = Math.floor(Math.random() * 12) + 2;
 
 function primerNumero(){
     alert (numeroRandomjugador)
-    console.log(numeroRandomjugador)
 }
 
 function segundoNumero(){
     alert (numeroRandompc)
-    console.log(numeroRandompc)
 }
 
 //condicional
 function compare(){
     if (numeroRandomjugador > numeroRandompc) {
-        console.log ("you win! \n congrats!")
         alert ("you win! \n congrats!")
       } else if (numeroRandomjugador == numeroRandompc) {
-        console.log ("it's a tie! \n :o")
         alert ("it's a tie! \n :o")
       } else{
         alert("you lost :( \n better luck next time")
-        console.log ("you lost :( \n better luck next time")
       }
 
 }
