a30e4fc65782b63b83be0b714969e45f57ddb598
gigi <giselaoliva99@hotmail.com>
2023-04-09 21:05:14 -0300

    puse más botones en vez del alert

--- dados.js
+++ dados.js
@@ -1,23 +1,37 @@
 
-//funcion para el numero del jugador//
-//document.getElementById("dadosjugador").addEventListener("click", numeroRandomjugador);
+//variables para ambos
+//numeros random que siempre comienzen desde 2 hasta el 12
+let numeroRandomjugador = Math.floor(Math.random()* 12) + 2;
+let numeroRandompc = Math.floor(Math.random() * 12) + 2; 
 
-function numeroRandomjugador(){
-    Math.floor(Math.random() *12 + 1);
-    }
-    
+//funciones para mostrar los numeros generados en el alert
 
-//funcion para el resultado de la pc//
+function primerNumero(){
+    alert (numeroRandomjugador)
+    console.log(numeroRandomjugador)
+}
 
+function segundoNumero(){
+    alert (numeroRandompc)
+    console.log(numeroRandompc)
+}
 
+//condicional
+function compare(){
+    if (numeroRandomjugador > numeroRandompc) {
+        console.log ("you win! \n congrats!")
+        alert ("you win! \n congrats!")
+      } else if (numeroRandomjugador == numeroRandompc) {
+        console.log ("it's a tie! \n :o")
+        alert ("it's a tie! \n :o")
+      } else{
+        alert("you lost :( \n better luck next time")
+        console.log ("you lost :( \n better luck next time")
+      }
 
-//function resultado(){
-    
-//}
+}
 
-//funcion para el mensaje a recibir dependiendo del resultado
-//if: mayor que la pc
-//else: menor que la pc
-
-//let numero = document.getElementById()
-
+//refresca la página
+function refreshGame(){
+    location.reload();
+}
